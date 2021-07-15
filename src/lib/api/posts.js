1aef5b2a9c5f021916f324101c2ca3552699dfef
seoyeonko <seoyeon99o@naver.com>
2021-07-15 19:59:30 +0900

    Feat: 포스트 수정

--- src/lib/api/posts.js
+++ src/lib/api/posts.js
@@ -15,3 +15,10 @@ export const listPosts = ({ page, username, tag }) => {
   return client.get(`/api/posts?${queryString}`);
 };
 // API 호출시 파라미터값 넣으면, /api/posts?username=tester&page=2 주소 형태
+
+export const updatePost = ({ id, title, body, tags }) =>
+  client.patch(`/api/posts/${id}`, {
+    title,
+    body,
+    tags,
+  });
